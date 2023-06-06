7726c4251b4911ef2872e147c8b4e573ab021ceb
Sushma <88388730+Sushma-Soni@users.noreply.github.com>
2023-06-06 14:14:22 +0530

    Add files via upload

---
+++ Full_sub/fsdata.v
@@ -0,0 +1,24 @@
+// Code your testbench here
+// or browse Examples
+module full_subtest ();
+  reg a;
+  reg b;
+  reg cin;
+  wire diff;
+  wire borrow;
+  
+  full_sub
+  uut(.a(a),.b(b),.cin(cin),.diff(diff),.borrow(borrow));
+  initial begin
+   #5 a = 2'b00 ; b = 2'b11; cin = 2'b00 ;
+   #5 a = 2'b01 ; b = 2'b10; cin = 2'b01 ;
+   #5 a = 2'b10 ; b = 2'b01; cin = 2'b10 ;
+   #5 a = 2'b11 ; b = 2'b00; cin = 2'b11 ;
+  
+  end
+  initial begin
+    $dumpfile("full-sub.vcd");
+    $dumpvars(0,full_subtest );
+  end
+endmodule
+  
