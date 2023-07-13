ab12450d06578c2adc3a988b1de0b907f5e542e7
Sushma <88388730+Sushma-Soni@users.noreply.github.com>
2023-07-13 16:48:17 +0530

    Update mux2_1test.v

--- mux2_1/mux2_1test.v
+++ mux2_1/mux2_1test.v
@@ -5,10 +5,8 @@ module muxtest2_1();
   mux2_1 uut (.a(A),.b(B),.s(S),.y(Y));
   
   initial begin
-    #5 A= 2'b00 ; B =  2'b00; S= 0;
-    #5 A= 2'b10 ; B =  2'b01;S=1;
-    #5 A= 2'b01 ; B =  2'b11; S= 0;
-    #5 A= 2'b11 ; B =  2'b00;S=1;
+    #5 A= 1'b0 ; B =  1'b0; S= 0;
+    #5 A= 1'b1; B =  1'b1;S=1;
     #5 $finish ;
    end
   initial begin
